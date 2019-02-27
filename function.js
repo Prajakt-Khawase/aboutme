d8d754284bb9755686d64c322070c926ad17b3c5
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-02-27 14:17:00 -0600

    declared the clickable buttonelement

--- function.js
+++ function.js
@@ -4,3 +4,6 @@ console.log('INFO: Getting access to elements');
 const lengthElement = document.getElementById("length");
 const heightElement = document.getElementById("height");
 const widthElement = document.getElementById("width");
+
+
+const btnElement = document.getElementById("calcbtn");
