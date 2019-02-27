2d3d9d1e0f2d58b5848b1ee5bc78286f879a2fcf
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-02-27 14:22:39 -0600

    As directed Adding the local event handlers

--- function.js
+++ function.js
@@ -17,3 +17,11 @@ const width = widthElement.value;
 console.log("length", length);
 console.log("height", height);
 console.log("width", width);
+
+console.log('INFO: Configuring event handlers');
+
+//Storing Locally as Guided by Dr. Case
+window.addEventListener('load', (event) => {
+  console.log(' Starting initialization!')
+  if (localStorage.getItem('length')){
+    document.getElementById('length').value = parseInt(localStorage.length)
